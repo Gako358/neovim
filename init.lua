f1870bd7321165c63e70af36988e42b4e1259880
Gako358 <gako358@outlook.com>
2022-07-25 16:14:41 +0200

    debugging

--- init.lua
+++ init.lua
@@ -1,14 +1,7 @@
-local utils = require("core.utils")
+-----------------------------------------------------------
+-- Color schemes configuration file
+-----------------------------------------------------------
 
-pcall(require, "impatient")
-
-for _, module_name in
-  ipairs({ "core.colors", "core.keymaps", "core.settings" })
-do
-  local ok, err = pcall(require, module_name)
-  if not ok then
-    local msg = "calling module: " .. module_name .. " fail: " .. err
-    utils.errorL(msg)
-  end
-end
-
+-- Colorscheme Configurations
+-- Lua}
+require('github-theme').setup()
