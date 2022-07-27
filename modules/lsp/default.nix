eef9cd5d6ee29cec89f2550244d932e04914a649
Gako358 <gako358@outlook.com>
2022-07-27 12:15:31 +0200

    continue on lsp config

--- modules/lsp/default.nix
+++ modules/lsp/default.nix
@@ -13,17 +13,34 @@
     };
 
     config = mkIf cfg.enable {
+      environment.systemPackages = with pkgs; [
+       rnix-lsp
+       clang-tools
+       rust-analyzer
+       nodePackages.bash
+       nodePackages.pyright
+       sumneko-lua-language-server
+
+      ];
+
       vim.startPlugins = with pkgs.neovimPlugins; [
         nvim-lspconfig
-        nvim-lsp-installer
       ];
 
       vim.luaConfigRC = let
       in ''    
+        local opts = { noremap=true, silent=true }
+        vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
+        vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+        vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+        vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
+        
+        -- Use an on_attach function to only map the following keys
+        -- after the language server attaches to the current buffer
         local on_attach = function(client, bufnr)
           -- Enable completion triggered by <c-x><c-o>
           vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-
+        
           -- Mappings.
           -- See `:help vim.lsp.*` for documentation on any of the below functions
           local bufopts = { noremap=true, silent=true, buffer=bufnr }
@@ -43,166 +60,27 @@
           vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
           vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, bufopts)
         end
-
-        local capabilities = vim.lsp.protocol.make_client_capabilities()
-        capabilities.textDocument.completion.completionItem = {
-          documentationFormat = {
-            "markdown",
-            "plaintext",
-          },
-          snippetSupport = true,
-          preselectSupport = true,
-          insertReplaceSupport = true,
-          labelDetailsSupport = true,
-          deprecatedSupport = true,
-          commitCharactersSupport = true,
-          tagSupport = {
-            valueSet = { 1 },
-          },
-          resolveSupport = {
-            properties = {
-              "documentation",
-              "detail",
-              "additionalTextEdits",
-            },
-          },
+        
+        local lsp_flags = {
+          -- This is the default in Nvim 0.7+
+          debounce_text_changes = 150,
         }
-        
-        -- Add neovim built-in Lua library into lsp runtime path.
-        local neovim_lua_runtime_settings = {
-          Lua = {
-            runtime = {
-              -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-              version = "LuaJIT",
-            },
-            diagnostics = {
-              -- Get the language server to recognize the `vim` global
-              globals = { "vim" },
-            },
-            workspace = {
-              -- Make the server aware of Neovim runtime files
-              library = vim.api.nvim_get_runtime_file("", true),
-            },
-            -- Do not send telemetry data containing a randomized but unique identifier
-            telemetry = {
-              enable = false,
-            },
-          },
-        }
-        
-        -- Setup border for the floating window
-        local handlers = {
-          ["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-            border = "single",
-          }),
-        
-          ["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
-            border = "single",
-          }),
-        }
-        
-        -- Setup diagnostic icons and signs
-        vim.diagnostic.config({
-          virtual_text = {
-            prefix = "﮿",
-            spacing = 4,
-            source = "always",
-          },
-          signs = true,
-          underline = true,
-          -- update diagnostic in insert mode will be annoying when the output is too verbose
-          update_in_insert = false,
-        })
-        
-        local signs = {
-          Error = " ",
-          Warn = " ",
-          Hint = " ",
-          Info = " ",
-        }
-        
-        for type, icon in pairs(signs) do
-          local hl = "DiagnosticSign" .. type
-          vim.fn.sign_define(hl, {
-            text = icon,
-            texthl = hl,
-            numhl = "",
-          })
-        end
-        
-        -- [[ =================================================================================
-        --  LSP SETUP MAIN LOGIC
-        -- =================================================================================]]
-        
-        require("nvim-lsp-installer").setup({
-          -- only ensure Lua language server is installed
-          automatic_installation = true,
-          ui = {
-            icons = {
-              server_installed = "✓",
-              server_pending = "➜",
-              server_uninstalled = "✗",
-            },
-          },
-        })
-        
-        -- Preconfigured language server that will be **automatically** installed.
-        --
-        -- WARNING: rust-analyzer is set up by plugin "rust-tools.nvim", *DONT*
-        -- configured it manually here.
-        --
-        -- User might define same server for multiple filetype, so here we use Set data structure
-        -- for unique value.
-        --
-        -- always enable sumneko_lua for Lua developing
-        local server_set = {
-          sumneko_lua = 0,
-        }
-        
-        -- IF
-        --   * we have a custom.lua file
-        --   * custom.lua file return a table which contains `langs` field
-        --   * the `langs` field has a Lua table value
-        --   * and the size of the `langs` field is not zero
-        -- THEN
-        --   we install and configure those language server
-        if have_custom and custom.langs and type(custom.langs) == "table" and #custom.langs > 0 then
-          -- Insert a value into table if it is not presented in that table
-          -- @field server: string
-          function server_set:push(server)
-            if self[server] == nil then
-              self[server] = 0
-            end
-          end
-        
-          for _, lang in ipairs(custom.langs) do
-            -- { "language filetype", "language server" }
-            if type(lang) == "table" and #lang > 1 then
-              -- lua 5.1 doesn't have continue keyword, so we have to write nested if block
-              server_set:push(lang[2])
-            end
-          end
-        
-          server_set.push = nil
-        end
-        
-        -- Attach the above settings to all the lspservers. And tell the nvim-lsp-installer to
-        -- install those servers when necessary.
-        for lspserver, _ in pairs(server_set) do
-          local opts = {
+        require('lspconfig')['pyright'].setup{
             on_attach = on_attach,
-            capabilities = capabilities,
-            root_dir = vim.loop.cwd,
-            handlers = handlers,
-          }
-        
-          if lspserver == "sumneko_lua" then
-            opts.settings = neovim_lua_runtime_settings
-          end
-        
-          lspconfig[lspserver].setup(opts)
-          vim.cmd([[ do User LspAttachBuffers ]])
-        end
-    '';
+            flags = lsp_flags,
+        }
+        require('lspconfig')['bash'].setup{
+            on_attach = on_attach,
+            flags = lsp_flags,
+        }
+        require('lspconfig')['rust_analyzer'].setup{
+            on_attach = on_attach,
+            flags = lsp_flags,
+            -- Server-specific settings...
+            settings = {
+              ["rust-analyzer"] = {}
+            }
+        }
+      '';
   };
 }
