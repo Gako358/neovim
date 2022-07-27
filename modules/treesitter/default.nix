56c42809da5e75b1b2efbf1e926cfce3fe2edbaa
Gako358 <gako358@outlook.com>
2022-07-27 13:41:42 +0200

    treesitter parser error

--- modules/treesitter/default.nix
+++ modules/treesitter/default.nix
@@ -17,62 +17,54 @@ in {
       nvim-treesitter
     ];
     vim.luaConfigRC = let
-      tree-sitter-hare = builtins.fetchGit {
-        url = "https://git.sr.ht/~ecmma/tree-sitter-hare";
-        ref = "master";
-        rev = "bc26a6a949f2e0d98b7bfc437d459b250900a165";
-      };
     in ''
       -- Treesitter config
-      require'nvim-treesitter.configs'.setup {
+      require("nvim-treesitter.configs").setup({
         ensure_installed = {
-        vim.g.enable_treesitter_ft,
-        'typescript',
-        'javascript',
-        'comment',
-        'python',
-        'bibtex',
-        'html',
-        'bash',
-        'make',
-        'json',
-        'rust',
-        'nix',
-        'vim',
-        'lua',
-        'css',
-        'cpp',
-        'c',
+          vim.g.enable_treesitter_ft,
+          'typescript',
+          'javascript',
+          'comment',
+          'python',
+          'bibtex',
+          'html',
+          'bash',
+          'make',
+          'json',
+          'rust',
+          'vim',
+          'lua',
+          'css',
+          'cpp',
+          'c',
         },
-
         highlight = {
           enable = true,
-          disable = {},
         },
-
         matchup = {
           enable = true,
         },
+        textobjects = {
+          select = {
+            enable = true,
 
-        incremental_selection = {
-          enable = true,
-          keymaps = {
-            init_selection = "gnn",
-            node_incremental = "grn",
-            scope_incremental = "grc",
-            node_decremental = "grm",
+            -- Automatically jump forward to textobj, similar to targets.vim
+            lookahead = true,
+
+            keymaps = {
+              -- You can use the capture groups defined in textobjects.scm
+              ["af"] = "@function.outer",
+              ["if"] = "@function.inner",
+              ["ac"] = "@class.outer",
+              ["ic"] = "@class.inner",
+              ["ab"] = "@block.outer",
+              ["ib"] = "@block.inner",
+              ["al"] = "@call.outer",
+              ["il"] = "@call.inner",
+            },
           },
-        }
-      }
-
-      local parser_config = require'nvim-treesitter.parsers'.get_parser_configs()
-       parser_config.hare = {
-        install_info = {
-          url = "",
-          files = { "" }
         },
-        filetype = "ha",
-      }
+      })
     '';
   };
 }
