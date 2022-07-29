a7ca75329958ed6a30a75077c3abbb06765af818
gako358 <gako358@outlook.com>
2022-07-29 10:48:28 +0200

    testing output for treesitter

--- modules/cmp/default.nix
+++ modules/cmp/default.nix
@@ -187,24 +187,6 @@ in {
         
         --set max height of items
         vim.cmd([[ set pumheight=7 ]])
-        --set highlights
-        local highlights = {
-          CmpItemKindText = { fg = "LightGrey" },
-          CmpItemKindFunction = { fg = "#C586C0" },
-          CmpItemKindClass = { fg = "Orange" },
-          CmpItemKindKeyword = { fg = "#f90c71" },
-          CmpItemKindSnippet = { fg = "#565c64" },
-          CmpItemKindConstructor = { fg = "#ae43f0" },
-          CmpItemKindVariable = { fg = "#9CDCFE", bg = "NONE" },
-          CmpItemKindInterface = { fg = "#f90c71", bg = "NONE" },
-          CmpItemKindFolder = { fg = "#2986cc" },
-          CmpItemKindReference = { fg = "#922b21" },
-          CmpItemKindMethod = { fg = "#C586C0" },
-          CmpItemMenu = { fg = "#C586C0", bg = "#C586C0" },
-          CmpItemAbbr = { fg = "#565c64", bg = "NONE" },
-          CmpItemAbbrMatch = { fg = "#569CD6", bg = "NONE" },
-          CmpItemAbbrMatchFuzzy = { fg = "#569CD6", bg = "NONE" },
-        }
         vim.api.nvim_set_hl(0, "CmpBorderedWindow_FloatBorder", { fg = "#565c64" })
         for group, hl in pairs(highlights) do
           vim.api.nvim_set_hl(0, group, hl)
--- modules/treesitter/default.nix
+++ modules/treesitter/default.nix
@@ -22,38 +22,51 @@ in {
       vim.startPlugins = with pkgs.neovimPlugins; [
         nvim-treesitter
       ];
-      
-      vim.luaConfigRC = ''
-        require'nvim-treesitter.configs'.setup({
-          ensure_installed = {
-            vim.g.enable_treesitter_ft,
-            'typescript',
-            'javascript',
-            'comment',
-            'python',
-            'bibtex',
-            'html',
-            'bash',
-            'make',
-            'json',
-            'rust',
-            'vim',
-            'lua',
-            'css',
-            'cpp',
-            'c',
-          },
+
+      vim.configRC = writeIf cfg.fold ''
+        " Tree-sitter based folding
+        set foldmethod=expr
+        set foldexpr=nvim_treesitter#foldexpr()
+        set nofoldenable
+      '';
+
+      vim.luaConfigRC = let
+        tree-sitter-hare = builtins.fetchGit {
+          url = "https://git.sr.ht/~ecmma/tree-sitter-hare";
+          ref = "master";
+          rev = "bc26a6a949f2e0d98b7bfc437d459b250900a165";
+        };
+      in ''
+        -- Treesitter config
+        require'nvim-treesitter.configs'.setup {
           highlight = {
             enable = true,
+            disable = {},
           },
+          incremental_selection = {
+            enable = true,
+            keymaps = {
+              init_selection = "gnn",
+              node_incremental = "grn",
+              scope_incremental = "grc",
+              node_decremental = "grm",
+            },
+          },
+          ${writeIf cfg.autotagHtml ''
           autotag = {
             enable = true,
           },
-          matchup = {
-            enable = true,
-          }
-        })
-      '';
+        ''}
+        }
+        local parser_config = require'nvim-treesitter.parsers'.get_parser_configs()
+        parser_config.hare = {
+          install_info = {
+            url = "",
+            files = { "" }
+          },
+          filetype = "ha",
+        }
+      '';     
     }
   );
 }
