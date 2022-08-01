653aaa0190b4354148ffa7d8092b72c01363de91
gako358 <gako358@outlook.com>
2022-08-01 20:56:07 +0200

    re compiling treesitter with gcc

--- modules/treesitter/default.nix
+++ modules/treesitter/default.nix
@@ -21,7 +21,6 @@ in {
     in {  
       vim.startPlugins = with pkgs.neovimPlugins; [
         nvim-treesitter
-        (nvim-treesitter.withPlugins (plugins: unstable.tree-sitter.allGrammars))
       ];
 
       vim.configRC = ''
