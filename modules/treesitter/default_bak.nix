471103a7167f9acae169b3221a5c9f565c7bb180
gako358 <gako358@outlook.com>
2022-08-01 10:28:29 +0200

    debuggins treesitter

---
+++ modules/treesitter/default_bak.nix
@@ -0,0 +1,59 @@
+{ pkgs, config, lib, ...}:
+with lib;
+with builtins;
+
+let
+  cfg = config.vim.treesitter;
+in {
+  options.vim.treesitter = {
+    enable = mkOption {
+      type = types.bool;
+      description = "enable tree-sitter [nvim-treesitter]";
+    };
+  };
+
+  config = mkIf cfg.enable (
+    let
+      writeIf = cond: msg:
+      if cond
+      then msg
+      else "";
+    in {  
+      vim.startPlugins = with pkgs.neovimPlugins; [
+        nvim-treesitter
+      ];
+
+      vim.configRC = ''
+        " Tree-sitter based folding
+        set foldmethod=expr
+        set foldexpr=nvim_treesitter#foldexpr()
+        set nofoldenable
+      '';
+
+      vim.luaConfigRC = ''
+        require("nvim-treesitter.install").compilers = { "clang++" }
+
+        -- Treesitter config
+        require'nvim-treesitter.configs'.setup {
+          ensure_installed = {
+            vim.g.enable_treesitter_ft,
+            'nix',
+          },
+          highlight = {
+            enable = true,
+            disable = {},
+          },
+          incremental_selection = {
+            enable = true,
+            keymaps = {
+              init_selection = "gnn",
+              node_incremental = "grn",
+              scope_incremental = "grc",
+              node_decremental = "grm",
+            },
+          },
+        }
+      '';     
+    }
+  );
+}
