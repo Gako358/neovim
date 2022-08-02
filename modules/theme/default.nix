8bd5da31fe5630050cac6bc6ac5275337478c14f
gako358 <gako358@outlook.com>
2022-08-02 23:42:54 +0200

    darker theme

--- modules/theme/default.nix
+++ modules/theme/default.nix
@@ -23,7 +23,7 @@ in {
       vim.luaConfigRC = ''
         require('onedark').setup  {
             -- Main options --
-            style = 'dark', -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
+            style = 'darker', -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
             transparent = false,  -- Show/hide background
             term_colors = true, -- Change terminal color as per the selected theme style
             ending_tildes = false, -- Show the end-of-buffer tildes. By default they are hidden
