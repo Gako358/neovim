13e62f558c0783dd1e31eb6919a38f779f9579be
gako358 <gako358@outlook.com>
2022-08-02 10:11:52 +0200

    trying again...

--- flake.nix
+++ flake.nix
@@ -130,13 +130,31 @@
       neovim-nightly = neovim.defaultPackage.${top.system};
     };
 
-    buildPluginOverlay = lib.buildPluginOverlay;
-
     pluginOverlay = top: last: let
+      treesitterGrammars = last.tree-sitter.withPlugins (p: [
+        p.tree-sitter-c
+        p.tree-sitter-nix
+        p.tree-sitter-python
+        p.tree-sitter-rust
+        p.tree-sitter-markdown
+        p.tree-sitter-comment
+        p.tree-sitter-toml
+        p.tree-sitter-make
+        p.tree-sitter-html
+        p.tree-sitter-javascript
+        p.tree-sitter-css
+      ]);
       buildPlug = name: top.vimUtils.buildVimPluginFrom2Nix {
         pname = name;
         version = "master";
         src = builtins.getAttr name inputs;
+        postPatch =
+        if (name == "nvim-treesitter")
+        then ''
+          rm -r parser
+          ln -s ${treesitterGrammars} parser
+        ''
+        else "";
       };
     in {
       neovimPlugins = builtins.listToAttrs (map (name: { inherit name; value = buildPlug name; }) plugins);
@@ -147,7 +165,6 @@
       cfg = { };
       overlays = [
         pluginOverlay
-        buildPluginOverlay
         externalBitsOverlay
       ];
     };
