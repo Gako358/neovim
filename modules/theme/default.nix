129c1ee69e17231435c6b216639c2996fd1e9745
gako358 <gako358@outlook.com>
2022-08-03 12:34:23 +0200

    removing diagnistic bg

--- modules/theme/default.nix
+++ modules/theme/default.nix
@@ -54,7 +54,7 @@ in {
             diagnostics = {
                 darker = true, -- darker colors for diagnostic
                 undercurl = true,   -- use undercurl instead of underline for diagnostics
-                background = true,    -- use background color for virtual text
+                background = false,    -- use background color for virtual text
             },
         }
         require('onedark').load()
