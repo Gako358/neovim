f221491c246206854c79f0538d26bd5d48daa374
gako358 <gako358@outlook.com>
2022-08-03 10:41:28 +0200

    fixing blanklines

--- modules/blanklines/default.nix
+++ modules/blanklines/default.nix
@@ -26,10 +26,9 @@ in {
 
       vim.luaConfigRC = ''
         vim.opt.list = true
-        vim.opt.listchars:append "eol:"""
         
         require("indent_blankline").setup {
-            show_end_of_line = true,
+
         }
       '';
     }
