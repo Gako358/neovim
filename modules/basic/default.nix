7ef702a25c03631a766676c93d5dd9b346f1a0e3
gako358 <gako358@outlook.com>
2022-08-13 12:40:03 +0200

    fixing zathura as reader, gnome reader not suported

--- modules/basic/default.nix
+++ modules/basic/default.nix
@@ -180,7 +180,7 @@ in {
       -----------------------------------------------------------
       -- VimTeX Management
       -----------------------------------------------------------
-      vim.g.vimtex_view_method = 'evince'
+      vim.g.vimtex_view_method = 'zathura'
       vim.g.vimtex_compiler_method = 'latexmk'
       vim.g.tex_flavor = 'latex'
       vim.g.vimtex_quickfix_mode = 1
