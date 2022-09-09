121388caf23a7b11a95ec5592695aff03c4c4be1
gako358 <gako358@outlook.com>
2022-09-09 10:24:01 +0200

    keybinds not working, debugging

--- modules/toggleterm/default.nix
+++ modules/toggleterm/default.nix
@@ -25,14 +25,6 @@ in {
 
       vim.luaConfigRC = ''
         require("toggleterm").setup()
-          autocmd TermEnter term://*toggleterm#*
-            \ tnoremap <silent><c-t> <Cmd>exe v:count1 . "ToggleTerm"<CR>
-
-          " By applying the mappings this way you can pass a count to your
-          " mapping to open a specific window.
-          " For example: 2<C-t> will open terminal 2
-          nnoremap <silent><c-t> <Cmd>exe v:count1 . "ToggleTerm"<CR>
-          inoremap <silent><c-t> <Esc><Cmd>exe v:count1 . "ToggleTerm"<CR>
       '';
     }
   );
