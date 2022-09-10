401f5efd78b753fa352f14e53a2e8c4a680c0f12
gako358 <gako358@outlook.com>
2022-09-10 11:48:28 +0200

    removed dup formatting and formatted lsp

--- modules/lsp/lsp.nix
+++ modules/lsp/lsp.nix
@@ -1,46 +1,55 @@
- { pkgs, config, lib, ...}:
-  with lib;
-  with builtins;
-
-  let
-    cfg = config.vim.lsp;
-  in {
-    options.vim.lsp = {
-      enable = mkOption {
-        type = types.bool;
-        description = "enable lsp config [nvim-lspconfig]";
-      };
-      formatOnSave = mkEnableOption "format on save";
-
-      python = mkEnableOption "Enable Python Language Support";
-      clang = mkEnableOption "Enable C Language Support";
-      bash = mkEnableOption "Enable Bash Language Support";
-      lua = mkEnableOption "Enable Lua Language Support";
-      nix = mkEnableOption "Enable Nix Language Support";
-      rust = mkEnableOption "Enable Rust Support";
-      typescript = mkEnableOption "Enable Typescript/Javascript Support";
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}:
+with lib;
+with builtins; let
+  cfg = config.vim.lsp;
+in {
+  options.vim.lsp = {
+    enable = mkOption {
+      type = types.bool;
+      description = "enable lsp config [nvim-lspconfig]";
     };
+    formatOnSave = mkEnableOption "format on save";
 
-    config = mkIf cfg.enable {
-      vim.startPlugins = with pkgs.neovimPlugins; [
+    python = mkEnableOption "Enable Python Language Support";
+    clang = mkEnableOption "Enable C Language Support";
+    bash = mkEnableOption "Enable Bash Language Support";
+    lua = mkEnableOption "Enable Lua Language Support";
+    nix = mkEnableOption "Enable Nix Language Support";
+    rust = mkEnableOption "Enable Rust Support";
+    typescript = mkEnableOption "Enable Typescript/Javascript Support";
+  };
+
+  config = mkIf cfg.enable {
+    vim.startPlugins = with pkgs.neovimPlugins;
+      [
         nvim-lspconfig
         null-ls
       ]
       ++ (
-        if cfg.rust then [
+        if cfg.rust
+        then [
           rust-tools
           crates-nvim
         ]
         else []
       );
 
-      vim.configRC = ''
-        ${if cfg.nix then ''
+    vim.configRC = ''
+      ${
+        if cfg.nix
+        then ''
           autocmd filetype nix setlocal tabstop=2 shiftwidth=2 softtabstop=2
         ''
         else ""
-        } 
-        ${if cfg.rust then ''
+      }
+      ${
+        if cfg.rust
+        then ''
           function! MapRustTools()
             nnoremap <silent><leader>rh <cmd>lua require('rust-tools').hover_actions.hover_actions()<CR>
             nnoremap <silent><leader>ri <cmd>lua require('rust-tools').inlay_hints.set()<CR>
@@ -59,138 +68,172 @@
             autocmd filetype rust nnoremap <silent><leader>rg <cmd>lua require('rust-tools.crate_graph').view_crate_graph('x11', nil)<CR>
         ''
         else ""
-        }
-      '';
+      }
+    '';
 
-      vim.luaConfigRC = let
-      in '' 
-        local lsp_flags = {
-          -- This is the default in Nvim 0.7+
-          debounce_text_changes = 150,
-        }
-        
-        local opts = { noremap=true, silent=true }
-        vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
-        vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
-        vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
-        vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
-        
-        -- Use an on_attach function to only map the following keys
-        -- after the language server attaches to the current buffer
-        local on_attach = function(client, bufnr)
-          -- Enable completion triggered by <c-x><c-o>
-          vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-        
-          -- Mappings.
-          -- See `:help vim.lsp.*` for documentation on any of the below functions
-          local bufopts = { noremap=true, silent=true, buffer=bufnr }
-          vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-          vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-          vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-          vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-          vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
-          vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-          vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
-          vim.keymap.set('n', '<space>wl', function()
-            print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-          end, bufopts)
-          vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
-          vim.keymap.set('n', '<space>gF', vim.lsp.buf.formatting, bufopts)
-          vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
-          vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
-          vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-          vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, bufopts)
-        end
+    vim.luaConfigRC = let
+    in ''
+      local lsp_flags = {
+        -- This is the default in Nvim 0.7+
+        debounce_text_changes = 150,
+      }
 
-        -- Seting up null-ls
-        local null_ls = require("null-ls")
-        local null_helpers = require("null-ls.helpers")
-        local null_methods = require("null-ls.methods")
+      local opts = { noremap=true, silent=true }
+      vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
+      vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+      vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+      vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
 
-        local ls_sources = {
-          ${if cfg.python then ''
-            null_ls.builtins.formatting.black.with({
-                command = "${pkgs.black}/bin/black",
-              }),
-          ''else ""}
+      -- Use an on_attach function to only map the following keys
+      -- after the language server attaches to the current buffer
+      local on_attach = function(client, bufnr)
+        -- Enable completion triggered by <c-x><c-o>
+        vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
-          ${if cfg.nix then ''
-            null_ls.builtins.formatting.alejandra.with({
-                command = "${pkgs.alejandra}/bin/alejandra",
+        -- Mappings.
+        -- See `:help vim.lsp.*` for documentation on any of the below functions
+        local bufopts = { noremap=true, silent=true, buffer=bufnr }
+        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+        vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+        vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+        vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+        vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+        vim.keymap.set('n', '<space>wl', function()
+          print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+        end, bufopts)
+        vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+        vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+        vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+        vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+        vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, bufopts)
+      end
+
+      -- Seting up null-ls
+      local null_ls = require("null-ls")
+      local null_helpers = require("null-ls.helpers")
+      local null_methods = require("null-ls.methods")
+
+      local ls_sources = {
+        ${
+        if cfg.python
+        then ''
+          null_ls.builtins.formatting.black.with({
+              command = "${pkgs.black}/bin/black",
             }),
-          ''else ""}
+        ''
+        else ""
+      }
 
-          ${if cfg.typescript then ''
-            null_ls.builtins.diagnostics.eslint,
-            null_ls.builtins.formatting.prettier,
-          ''else ""} 
-        }
+        ${
+        if cfg.nix
+        then ''
+          null_ls.builtins.formatting.alejandra.with({
+              command = "${pkgs.alejandra}/bin/alejandra",
+          }),
+        ''
+        else ""
+      }
 
-        vim.g.formatsave = ${if cfg.formatOnSave
-          then "true"
-          else "false"
-        };
+        ${
+        if cfg.typescript
+        then ''
+          null_ls.builtins.diagnostics.eslint,
+          null_ls.builtins.formatting.prettier,
+        ''
+        else ""
+      }
+      }
 
-        -- Enable formatting
-        format_callback = function(client, bufnr)
-          vim.api.nvim_create_autocmd("BufWritePre", {
-            group = augroup,
-            buffer = bufnr,
-            callback = function()
-              if vim.g.formatsave then
-                  local params = require'vim.lsp.util'.make_formatting_params({})
-                  client.request('textDocument/formatting', params, nil, bufnr)
-              end
+      vim.g.formatsave = ${
+        if cfg.formatOnSave
+        then "true"
+        else "false"
+      };
+
+      -- Enable formatting
+      format_callback = function(client, bufnr)
+        vim.api.nvim_create_autocmd("BufWritePre", {
+          group = augroup,
+          buffer = bufnr,
+          callback = function()
+            if vim.g.formatsave then
+                local params = require'vim.lsp.util'.make_formatting_params({})
+                client.request('textDocument/formatting', params, nil, bufnr)
             end
-          })
-        end
-
-        -- Enable null-ls
-        require('null-ls').setup({
-          diagnostics_format = "[#{m}] #{s} (#{c})",
-          debounce = 250,
-          default_timeout = 5000,
-          sources = ls_sources,
-          on_attach= on_attach
+          end
         })
+      end
 
-        ${if cfg.python then ''
+      -- Enable null-ls
+      require('null-ls').setup({
+        diagnostics_format = "[#{m}] #{s} (#{c})",
+        debounce = 250,
+        default_timeout = 5000,
+        sources = ls_sources,
+        on_attach= on_attach
+      })
+
+      ${
+        if cfg.python
+        then ''
           require('lspconfig')['pyright'].setup{
               on_attach = on_attach,
               flags = lsp_flags,
               cmd = {"${pkgs.nodePackages.pyright}/bin/pyright-langserver", "--stdio"}
           }
-        '' else ""}
-        ${if cfg.clang then ''
+        ''
+        else ""
+      }
+      ${
+        if cfg.clang
+        then ''
           require('lspconfig')['clangd'].setup{
               on_attach = on_attach,
               flags = lsp_flags,
               cmd = {'${pkgs.clang-tools}/bin/clangd', '--background-index'};
               filetypes = { "c", "cpp", "objc", "objcpp" };
           }
-        '' else ""}
-        ${if cfg.bash then ''
+        ''
+        else ""
+      }
+      ${
+        if cfg.bash
+        then ''
           require('lspconfig')['bashls'].setup{
               on_attach = on_attach,
               flags = lsp_flags,
               cmd = {"${pkgs.nodePackages.bash-language-server}/bin/bash-language-server", "start"}
           }
-        '' else ""}
-        ${if cfg.lua then ''
+        ''
+        else ""
+      }
+      ${
+        if cfg.lua
+        then ''
           require('lspconfig')['sumneko_lua'].setup{
               on_attach = on_attach,
               flags = lsp_flags,
               cmd = {"${pkgs.sumneko-lua-language-server}/bin/sumneko_lua", "start"}
           }
-        '' else ""}
-        ${if cfg.nix then ''
+        ''
+        else ""
+      }
+      ${
+        if cfg.nix
+        then ''
           require('lspconfig')['rnix'].setup{
               on_attach = on_attach,
               flags = lsp_flags,
               cmd = {"${pkgs.rnix-lsp}/bin/rnix-lsp"}
           }
-        '' else ""}
-        ${if cfg.rust then ''
+        ''
+        else ""
+      }
+      ${
+        if cfg.rust
+        then ''
           local opts = {
               tools = { -- rust-tools options
                   -- how to execute terminal commands
@@ -214,15 +257,21 @@
               flags = lsp_flags,
               cmd = {"${pkgs.rust-analyzer}/bin/rust-analyzer"}
           }
-        '' else ""}
-         
-        ${if cfg.typescript then ''
+        ''
+        else ""
+      }
+
+      ${
+        if cfg.typescript
+        then ''
           require('lspconfig')['tsserver'].setup{
               on_attach = on_attach,
               flags = lsp_flags,
               cmd = { "${pkgs.nodePackages.typescript-language-server}/bin/typescript-language-server", "--stdio" }
           }
-        '' else ""}
-      '';
+        ''
+        else ""
+      }
+    '';
   };
 }
