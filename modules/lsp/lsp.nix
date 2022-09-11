95fc3c9489b0af6ed033f2b1402144a31333adad
gako358 <gako358@outlook.com>
2022-09-11 11:43:15 +0200

    changing formatting api

--- modules/lsp/lsp.nix
+++ modules/lsp/lsp.nix
@@ -113,7 +113,7 @@ in {
         vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
         vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
         vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-        vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, bufopts)
+        vim.keymap.set('n', '<space>f', vim.lsp.buf.format, bufopts)
       end
 
       -- Seting up null-ls
