7696c449033b8d8bd234a0b20ffa4653bd878d68
gako358 <gako358@outlook.com>
2022-09-20 21:50:01 +0200

    removed dap

--- flake.nix
+++ flake.nix
@@ -44,10 +44,6 @@
       url = "github:simrat39/rust-tools.nvim";
       flake = false;
     };
-    nvim-dap = {
-      url = "github:mfussenegger/nvim-dap";
-      flake = false;
-    };
 
     # COPILOT Only needed on initial install
     github-copilot = {
@@ -166,7 +162,6 @@
       "null-ls"
       "trouble"
       "rust-tools"
-      "nvim-dap"
       "github-copilot"
       "nvim-cmp"
       "luasnip"
@@ -237,7 +232,6 @@
     allPkgs = lib.mkPkgs {
       inherit nixpkgs;
       cfg = {};
-      cfg.allowUnfree = true;
       overlays = [
         pluginOverlay
         externalBitsOverlay
