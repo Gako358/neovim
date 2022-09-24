7de7ed2473e474100fe5005fe8523b2f41b1fa46
gako358 <gako358@outlook.com>
2022-09-24 13:30:54 +0200

    debugging kanagawa

--- flake.nix
+++ flake.nix
@@ -302,7 +302,7 @@
 
             vim.visuals = {
               enable = true;
-              theme.scheme = "tokyonight;
+              theme.scheme = "tokyonight";
               nvimAutoPairs.enable = true;
               nvimWebDevicons.enable = true;
               lightSpeed.enable = true;
