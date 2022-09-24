94618dc771b5d4c44b5c02addf817ca5a5e6b4c1
gako358 <gako358@outlook.com>
2022-09-24 13:56:59 +0200

    fixing bg for kanagawa

--- modules/visuals/kanagawa.nix
+++ modules/visuals/kanagawa.nix
@@ -12,6 +12,12 @@ in
     vim.startPlugins = with pkgs.neovimPlugins; [ kanagawa ];
 
     vim.luaConfigRC = ''
+      local default_colors = require("kanagawa.colors").setup()
+      local my_colors = {
+          -- use the palette color name...
+          bg = "#272727",
+      }
+      require'kanagawa'.setup({ colors = my_colors })
       vim.cmd("colorscheme kanagawa")
     '';
   };
