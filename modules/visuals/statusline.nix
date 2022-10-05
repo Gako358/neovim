7ecf0e16f9777405737e45a0a76c6a130827d3f6
gako358 <gako358@outlook.com>
2022-10-05 21:23:46 +0200

    working on statusline

--- modules/visuals/statusline.nix
+++ modules/visuals/statusline.nix
@@ -14,74 +14,153 @@ in
 
   config = mkIf (cfg.enable) {
     vim.luaConfigRC = ''
-      -- Different modes
-      local modes = {
-        ["n"] = "NORMAL",
-        ["no"] = "NORMAL",
-        ["v"] = "VISUAL",
-        ["V"] = "VISUAL LINE",
-        ["␖"] = "VISUAL BLOCK",
-        ["s"] = "SELECT",
-        ["S"] = "SELECT LINE",
-        ["␓"] = "SELECT BLOCK",
-        ["i"] = "INSERT",
-        ["ic"] = "INSERT",
-        ["R"] = "REPLACE",
-        ["Rv"] = "VISUAL REPLACE",
-        ["c"] = "COMMAND",
-        ["cv"] = "VIM EX",
-        ["ce"] = "EX",
-        ["r"] = "PROMPT",
-        ["rm"] = "MOAR",
-        ["r?"] = "CONFIRM",
-        ["!"] = "SHELL",
-        ["t"] = "TERMINAL",
+      local fn = vim.fn
+      local api = vim.api
+
+      local set_hl = function(group, options)
+        local bg = options.bg == nil and "" or 'guibg=' .. options.bg
+        local fg = options.fg == nil and "" or 'guifg=' .. options.fg
+        local gui = options.gui == nil and "" or 'gui=' .. options.gui
+
+        vim.cmd(string.format('hi %s %s %s %s', group, bg, fg, gui))
+      end
+
+      -- you can of course pick whatever colour you want, I picked these colours
+      -- because I use Gruvbox and I like them
+      local highlights = {
+        {'StatusLine', { fg = '#363646', bg = '#DCD7BA' }},
+        {'StatusLineNC', { fg = '#363646', bg = '#DCD7BA' }},
+        {'Mode', { bg = '#2D4F67', fg = '#1D2021', gui="bold" }},
+        {'LineCol', { bg = '#2D4F67', fg = '#1D2021', gui="bold" }},
+        {'Git', { bg = '#223249', fg = '#DCD7BA' }},
+        {'Filetype', { bg = '#223249', fg = '#DCD7BA' }},
+        {'Filename', { bg = '#363646', fg = '#DCD7BA' }},
+        {'ModeAlt', { bg = '#363646', fg = '#DCD7BA' }},
+        {'GitAlt', { bg = '#363646', fg = '#76946A' }},
+        {'LineColAlt', { bg = '#363646', fg = '#DCD7BA' }},
+        {'FiletypeAlt', { bg = '#363646', fg = '#DCD7BA' }},
+        {'diagnosticErr', { bg = '#E82424', fg = '#DCD7BA' }},
       }
 
-      -- Match all the modes
-      local function mode()
-        local current_mode = vim.api.nvim_get_mode().mode
-        return string.format(" %s ", modes[current_mode]):upper()
+      for _, highlight in ipairs(highlights) do
+        set_hl(highlight[1], highlight[2])
       end
 
-      -- Get the mode colors
-      local function update_mode_colors()
-        local current_mode = vim.api.nvim_get_mode().mode
-        local mode_color = "%#StatusLineAccent#"
-        if current_mode == "n" then
-            mode_color = "%#StatuslineAccent#"
-        elseif current_mode == "i" or current_mode == "ic" then
-            mode_color = "%#StatuslineInsertAccent#"
-        elseif current_mode == "v" or current_mode == "V" or current_mode == "␖" then
-            mode_color = "%#StatuslineVisualAccent#"
-        elseif current_mode == "R" then
-            mode_color = "%#StatuslineReplaceAccent#"
-        elseif current_mode == "c" then
-            mode_color = "%#StatuslineCmdLineAccent#"
-        elseif current_mode == "t" then
-            mode_color = "%#StatuslineTerminalAccent#"
+      local M = {}
+
+      -- possible values are 'arrow' | 'rounded' | 'blank'
+      local active_sep = 'blank'
+
+      -- change them if you want to different separator
+      M.separators = {
+        arrow = { '', '' },
+        rounded = { '', '' },
+        blank = { "", "" },
+      }
+
+      -- highlight groups
+      M.colors = {
+        active        = '%#StatusLine#',
+        inactive      = '%#StatusLineNC#',
+        file_name     = '%#Filename#',
+        mode          = '%#Mode#',
+        mode_alt      = '%#ModeAlt#',
+        git           = '%#Git#',
+        git_alt       = '%#GitAlt#',
+        filetype      = '%#Filetype#',
+        filetype_alt  = '%#FiletypeAlt#',
+        line_col      = '%#LineCol#',
+        line_col_alt  = '%#LineColAlt#',
+        diagnostic    = '%#diagnosticErr#',
+      }
+
+      M.trunc_width = setmetatable({
+        mode       = 80,
+        git_status = 90,
+        filename   = 140,
+        line_col   = 60,
+      }, {
+        __index = function()
+            return 80
         end
-        return mode_color
+      })
+
+      M.is_truncated = function(_, width)
+        local current_width = api.nvim_win_get_width(0)
+        return current_width < width
       end
 
-      -- Get the current file name
-      local function filepath()
-        local fpath = vim.fn.fnamemodify(vim.fn.expand "%", ":~:.:h")
-        if fpath == "" or fpath == "." then
-            return " "
+      M.modes = setmetatable({
+        ['n']  = {'Normal', 'N'};
+        ['no'] = {'N·Pending', 'N·P'} ;
+        ['v']  = {'Visual', 'V' };
+        ['V']  = {'V·Line', 'V·L' };
+        ['␖'] = {'V·Block', 'V·B'}; -- this is not ^V, but it's ␖, they're different
+        ['s']  = {'Select', 'S'};
+        ['S']  = {'S·Line', 'S·L'};
+        ['␓'] = {'S·Block', 'S·B'}; -- same with this one, it's not ^S but it's ␓
+        ['i']  = {'Insert', 'I'};
+        ['ic'] = {'Insert', 'I'};
+        ['R']  = {'Replace', 'R'};
+        ['Rv'] = {'V·Replace', 'V·R'};
+        ['c']  = {'Command', 'C'};
+        ['cv'] = {'Vim·Ex ', 'V·E'};
+        ['ce'] = {'Ex ', 'E'};
+        ['r']  = {'Prompt ', 'P'};
+        ['rm'] = {'More ', 'M'};
+        ['r?'] = {'Confirm ', 'C'};
+        ['!']  = {'Shell ', 'S'};
+        ['t']  = {'Terminal ', 'T'};
+      }, {
+        __index = function()
+            return {'Unknown', 'U'} -- handle edge cases
         end
-        return string.format(" %%<%s/", fpath)
-      end
+      })
 
-      local function filename()
-        local fname = vim.fn.expand "%:t"
-        if fname == "" then
-            return ""
+      M.get_current_mode = function(self)
+        local current_mode = api.nvim_get_mode().mode
+
+        if self:is_truncated(self.trunc_width.mode) then
+          return string.format(' %s ', self.modes[current_mode][2]):upper()
         end
-        return fname .. " "
+        return string.format(' %s ', self.modes[current_mode][1]):upper()
       end
 
-      local function lsp()
+      M.get_git_status = function(self)
+        -- use fallback because it doesn't set this variable on the initial `BufEnter`
+        local signs = vim.b.gitsigns_status_dict or {head = "", added = 0, changed = 0, removed = 0}
+        local is_head_empty = signs.head ~= ""
+
+        if self:is_truncated(self.trunc_width.git_status) then
+          return is_head_empty and string.format('  %s ', signs.head or "") or ""
+        end
+
+        return is_head_empty and string.format(
+          ' +%s ~%s -%s |  %s ',
+          signs.added, signs.changed, signs.removed, signs.head
+        ) or ""
+      end
+
+      M.get_filename = function(self)
+        if self:is_truncated(self.trunc_width.filename) then return " %<%f " end
+        return " %<%F "
+      end
+
+      M.get_filetype = function()
+        local file_name, file_ext = fn.expand("%:t"), fn.expand("%:e")
+        local icon = require'nvim-web-devicons'.get_icon(file_name, file_ext, { default = true })
+        local filetype = vim.bo.filetype
+
+        if filetype == "" then return "" end
+        return string.format(' %s %s ', icon, filetype):lower()
+      end
+
+      M.get_line_col = function(self)
+        if self:is_truncated(self.trunc_width.line_col) then return ' %l:%c ' end
+        return ' Ln %l, Col %c '
+      end
+
+      M.get_lsp_status = function()
         local count = {}
         local levels = {
           errors = "Error",
@@ -111,80 +190,52 @@ in
         return errors .. warnings .. hints .. info .. "%#Normal#"
       end
 
-      local function filetype()
-        return string.format(" %s ", vim.bo.filetype):upper()
+      M.set_active = function(self)
+        local colors = self.colors
+
+        local mode = colors.mode .. self:get_current_mode()
+        local mode_alt = colors.mode_alt .. self.separators[active_sep][1]
+        local git = colors.git .. self:get_git_status()
+        local git_alt = colors.git_alt .. self.separators[active_sep][1]
+        local lsp = colors.diagnostic .. self:get_lsp_status()
+        local filename = colors.file_name .. self:get_filename()
+        local filetype_alt = colors.filetype_alt .. self.separators[active_sep][2]
+        local filetype = colors.filetype .. self:get_filetype()
+        local line_col = colors.line_col .. self:get_line_col()
+        local line_col_alt = colors.line_col_alt .. self.separators[active_sep][2]
+
+        return table.concat({
+          colors.active, mode, mode_alt, git, git_alt,
+          "%=", filename, "%=",
+          filetype_alt, filetype, line_col_alt, line_col
+        })
       end
 
-      local function lineinfo()
-        if vim.bo.filetype == "alpha" then
-          return ""
-        end
-        return " %P %l:%c "
+      M.set_inactive = function(self)
+        return self.colors.inactive .. '%= %F %='
       end
 
-      -- Get the current git branch
-      local vcs = function()
-        local git_info = vim.b.gitsigns_status_dict
-        if not git_info or git_info.head == "" then
-          return ""
-        end
-        local added = git_info.added and ("%#GitSignsAdd#+" .. git_info.added .. " ") or ""
-        local changed = git_info.changed and ("%#GitSignsChange#~" .. git_info.changed .. " ") or ""
-        local removed = git_info.removed and ("%#GitSignsDelete#-" .. git_info.removed .. " ") or ""
-        if git_info.added == 0 then
-          added = ""
-        end
-        if git_info.changed == 0 then
-          changed = ""
-        end
-        if git_info.removed == 0 then
-          removed = ""
-        end
-        return table.concat {
-           " ",
-           added,
-           changed,
-           removed,
-           " ",
-           "%#GitSignsAdd# ",
-           git_info.head,
-           " %#Normal#",
-        }
+      M.set_explorer = function(self)
+        local title = self.colors.mode .. '   '
+        local title_alt = self.colors.mode_alt .. self.separators[active_sep][2]
+
+        return table.concat({ self.colors.active, title, title_alt })
       end
 
-      Statusline = {}
-      Statusline.active = function()
-        return table.concat {
-          "%#Statusline#",
-          update_mode_colors(),
-          mode(),
-          "%#Normal#",
-          filepath(),
-          filename(),
-          lsp(),
-          "%=%#StatusLineExtra#",
-          "%#Normal#",
-          vcs(),
-          filetype(),
-          lineinfo(),
-        }
-      end
-      function Statusline.inactive()
-        return " %F"
-      end
-      function Statusline.short()
-        return "%#StatusLineNC#   NvimTree"
-      end
+      Statusline = setmetatable(M, {
+        __call = function(statusline, mode)
+          if mode == "active" then return statusline:set_active() end
+          if mode == "inactive" then return statusline:set_inactive() end
+        end
+      })
 
-      -----------------------------------------------------------
-      -- Statusline enable
-      -----------------------------------------------------------
-      vim.api.nvim_exec([[
+      -- set statusline
+      -- TODO: replace this once we can define autocmd using lua
+      api.nvim_exec([[
         augroup Statusline
         au!
-        au WinEnter,BufEnter * setlocal statusline=%!v:lua.Statusline.active()
-        au WinLeave,BufLeave * setlocal statusline=%!v:lua.Statusline.inactive()
-        au WinEnter,BufEnter,FileType NvimTree setlocal statusline=%!v:lua.Statusline.short()
+        au WinEnter,BufEnter * setlocal statusline=%!v:lua.Statusline('active')
+        au WinLeave,BufLeave * setlocal statusline=%!v:lua.Statusline('inactive')
         augroup END
       ]], false)
     '';
