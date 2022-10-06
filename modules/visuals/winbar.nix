4c41ec46fa474c44e26c5399d9646cc9f9554071
gako358 <gako358@outlook.com>
2022-10-06 12:14:18 +0200

    set cmd height 0

--- modules/visuals/winbar.nix
+++ modules/visuals/winbar.nix
@@ -11,20 +11,21 @@ in
   config = mkIf (cfg.bar == "winbar") {
     vim.configRC = ''
       set laststatus=0
+      set cmdheight=0
     '';
 
     vim.luaConfigRC = ''
       local function status_line()
-        local file_name = "%-.15t"
         local modified = " %-m"
+        local file_type = " %y"
         local right_align = "%="
         local line_no = "%10([%l/%L%)]"
 
         return string.format(
           "%s%s%s%s",
-          file_name,
-          modified,
           right_align,
+          modified,
+          file_type,
           line_no
         )
       end
