f6c997374e0e8c12fa7a26cf00f5186619c40280
Gako358 <gako358@outlook.com>
2022-10-12 18:07:59 +0200

    updated plugins and made lsp_signature more usable

--- modules/lsp/lsp-signature.nix
+++ modules/lsp/lsp-signature.nix
@@ -19,7 +19,28 @@ in
 
     vim.luaConfigRC = ''
       -- Enable lsp signature viewer
-      require("lsp_signature").setup()
+      cfg = {
+        max_height = 7,
+        max_width = 55,
+        floating_window = true, -- show hint in a floating window, set to false for virtual text only mode
+
+        floating_window_above_cur_line = true, -- try to place the floating above the current line when possible Note:
+        floating_window_off_x = 1,
+        floating_window_off_y = 3,
+
+        handler_opts = {
+          border = "rounded"   -- double, rounded, single, shadow, none
+        },
+
+        always_trigger = false, -- sometime show signature on new line or in middle of parameter can be confusing, set it to false for #58
+
+        auto_close_after = nil,
+        extra_trigger_chars = {}, -- Array of extra characters that will trigger signature completion, e.g., {"(", ","}
+        zindex = 10, -- by default it will be on top of all floating windows, set to <= 50 send it to bottom
+      }
+
+      -- recommended:
+      require'lsp_signature'.setup(cfg)
     '';
   };
 }
