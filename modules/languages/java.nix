5ecff6c4d631c994a77c52d67e1ef8752be42312
Gako358 <gako358@outlook.com>
2024-05-25 21:10:12 +0200

    setting up java using jdtls start or attach

--- modules/languages/java.nix
+++ modules/languages/java.nix
@@ -21,12 +21,19 @@ with builtins; let
           local jdtls = require("jdtls")
           local root_markers = { ".git", "mvnw", "gradlew", "pom.xml", "build.gradle", ".project" }
           local root_dir = require("jdtls.setup").find_root(root_markers)
+
+          -- If root_dir is nil, use current working directory
+          if root_dir == nil then
+            root_dir = vim.fn.getcwd()
+          end
+
           local workspace_folder = home .. "/.cache/jdtls/" .. vim.fn.fnamemodify(root_dir, ":p:h:t")
 
-          -- Function to set root_dir to the parent directory of the .git folder
+          -- Setting up root_dir
           local function get_root_dir()
             return lspconfig.util.root_pattern(unpack(root_markers))(vim.fn.expand('%:p:h'))
           end
+
           local jdtls_config_dir = home .. "/.config/jdtls_config"
           os.execute("mkdir -p " .. jdtls_config_dir)
 
@@ -38,7 +45,7 @@ with builtins; let
           local debug_bundles = vim.split(vim.fn.glob("${pkgs.vscode-extensions.vscjava.vscode-java-debug}/share/vscode/extensions/vscjava.vscode-java-debug/server/*.jar"), "\n")
           local test_bundles = vim.split(vim.fn.glob("${pkgs.vscode-extensions.vscjava.vscode-java-test}/share/vscode/extensions/vscjava.vscode-java-test/server/*.jar"), "\n")
           vim.list_extend(bundles, debug_bundles)
-          -- vim.list_extend(bundles, test_bundles)
+          -- vim.list_extend(bundles, test_bundles) TODO: Need to look into
 
           java_on_attach = function(client, bufnr)
             attach_keymaps(client, bufnr)
@@ -49,14 +56,9 @@ with builtins; let
             vim.keymap.set("n", "<leader>jrc", "<Cmd>lua require'jdtls'.extract_constant()<CR>", opts)
             vim.keymap.set("x", "<leader>jrc", "<Esc><Cmd>lua require'jdtls'.extract_constant(true)<CR>", opts)
             vim.keymap.set("x", "<leader>jrm", "<Esc><Cmd>lua require'jdtls'.extract_method(true)<CR>", opts)
+
+            -- Set autocommands conditional on server_capabilities
             vim.lsp.codelens.refresh()
-
-            -- Commands
-            jdtls_setup.add_commands()
-
-            -- Dap config
-            jdtls.setup_dap({ hotcodereplace = 'auto' })
-            jdtls_dap.setup_dap_main_class_configs()
           end
 
           local config = {
@@ -122,7 +124,7 @@ with builtins; let
             "-configuration", jdtls_config_dir,
             "-data", workspace_folder,
           };
-          config.root_dir = get_root_dir
+          config.root_dir = get_root_dir()
           config.on_init = function(client, _)
             client.notify('workspace/didChangeConfiguration', { settings = config.settings })
           end
@@ -134,7 +136,20 @@ with builtins; let
             extendedClientCapabilities = extendedClientCapabilities;
           };
           config.filetypes = {"java"};
-          lspconfig.jdtls.setup(config)
+          local nvim_jdtls_group = vim.api.nvim_create_augroup("nvim-jdtls", { clear = true })
+
+          vim.api.nvim_create_autocmd(
+            "FileType",
+            {
+              pattern = { "java" },
+              callback = function()
+                jdtls.start_or_attach(config)
+                jdtls.setup_dap({ hotcodereplace = "auto" })
+                -- jdtls.dap.setup_dap_main_class_configs() TODO: return a nil
+              end,
+              group = nvim_jdtls_group,
+            }
+          )
         '';
     };
   };
--- modules/lsp/lspconfig.nix
+++ modules/lsp/lspconfig.nix
@@ -25,6 +25,28 @@ in {
 
       vim.luaConfigRC.lspconfig = nvim.dag.entryAfter ["lsp-setup"] ''
         local lspconfig = require('lspconfig')
+        lspconfig.lua_ls.setup {
+          on_init = function(client)
+            local path = client.workspace_folders[1].name
+            if not vim.loop.fs_stat(path .. '/.luarc.json') and not vim.loop.fs_stat(path .. '/.luarc.jsonc') then
+              client.config.settings = vim.tbl_deep_extend('force', client.config.settings, {
+                Lua = {
+                  runtime = {
+                    version = 'LuaJIT'
+                  },
+                  workspace = {
+                    checkThirdParty = false,
+                    library = {
+                      vim.env.VIMRUNTIME
+                    }
+                  }
+                }
+              })
+              client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
+            end
+            return true
+          end
+        }
       '';
     }
     {
